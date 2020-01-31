8ce5d0ac15fd848337e0c5ad72f9c4853bc1dd65
martin <martinezogo1987@gmail.com>
2020-01-30 21:23:11 -0600

    clicky

--- clickygame/src/components/Game/index.js
+++ clickygame/src/components/Game/index.js
@@ -1,33 +1,39 @@
 import React, { Component } from "react";
-class Game extends  Component {
-    state={ 
+class Game extends Component {
+    state = {
+
     
-        score:0,
+        score: 0,
         topScore: 0,
         photos: [
-            {src:"http://2.bp.blogspot.com/-jEvtYp2NVDU/UOQ9uCMXtpI/AAAAAAAAQQk/kA_elYotr3A/s1600/5299787-american-cocker-spaniel-puppy-with-paws-over-white-foreground.jpg",
-            id: "1"
-            }, 
-            {src:"http://blogs.uoregon.edu/yusugeog181/files/2014/05/Border-Collie-Puppy-0-1rzbtly.jpg",
-            id: "2"
+            {
+                src: "http://2.bp.blogspot.com/-jEvtYp2NVDU/UOQ9uCMXtpI/AAAAAAAAQQk/kA_elYotr3A/s1600/5299787-american-cocker-spaniel-puppy-with-paws-over-white-foreground.jpg",
+                id: "1"
             },
-            {src:"http://1.bp.blogspot.com/-5tB9q0VGQq4/VQmz8PQ0J1I/AAAAAAAAG4w/bnm6wibMRHc/s1600/richiemale.JPG",
-            id: "3"
+            {
+                src: "http://blogs.uoregon.edu/yusugeog181/files/2014/05/Border-Collie-Puppy-0-1rzbtly.jpg",
+                id: "2"
             },
-            {src:"http://1.bp.blogspot.com/-5tB9q0VGQq4/VQmz8PQ0J1I/AAAAAAAAG4w/bnm6wibMRHc/s1600/richiemale.JPG",
-            id: "4"
+            {
+                src: "http://1.bp.blogspot.com/-5tB9q0VGQq4/VQmz8PQ0J1I/AAAAAAAAG4w/bnm6wibMRHc/s1600/richiemale.JPG",
+                id: "3"
             },
-            
-            
+            {
+                src: "http://1.bp.blogspot.com/-5tB9q0VGQq4/VQmz8PQ0J1I/AAAAAAAAG4w/bnm6wibMRHc/s1600/richiemale.JPG",
+                id: "4"
+            },
         ]
-        
-    }
-    render(){
-        return(
-            <div>the scores </div>
 
-            
+    }
+    render() {
+        return (
+            <div>
+                {this.state.photos.map((photo) => {
+                    return (<img src={photo.src}></img>);
+                })}
+                
+            </div>
         )
     }
 }
-export default Game;
+export default Game
